360db153403af3f74007c9f1096cca27100849f9
Enrique <octepc.97@gmail.com>
2022-09-08 16:59:22 -0500

    added enviroment codes. Added the API

---
+++ src/components/CurrentWeather/index.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+
+export const CurrentWeather = () => {
+  useEffect(() => {
+    fetch(
+      `${process.env.REACT_APP_API}onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  return (
+    <section className="currentWeather">
+      <div className="currentWeather--details">
+        <h2>Actual Temp</h2>
+        <span>0°</span>
+        <ul className="currentWeather--stats">
+          <li>
+            <span>Wind</span>
+            <span>5 km/h</span>
+          </li>
+          <li>
+            <span>Humidity</span>
+            <span>24%</span>
+          </li>
+        </ul>
+        <div className="currentWeather--actual">
+          <h3>Raining</h3>
+        </div>
+      </div>
+      <div className="currentWeather--imgs"></div>
+    </section>
+  );
+};
