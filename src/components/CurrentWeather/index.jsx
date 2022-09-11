837017db5f0e0308e51f8944427dbaab34f22663
Enrique <octepc.97@gmail.com>
2022-09-11 13:43:57 -0500

    fixed the API bug

--- src/components/CurrentWeather/index.jsx
+++ src/components/CurrentWeather/index.jsx
@@ -1,41 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export const CurrentWeather = () => {
+  const [weather, setWeather] = useState(null);
+
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     fetch(
-      `${process.env.REACT_APP_API}onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${process.env.REACT_APP_API_KEY}`
+      `${process.env.REACT_APP_API}weather?lat=44.34&lon=10.99&appid=${process.env.REACT_APP_API_KEY}`
     )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        setWeather(data);
+        console.log(data);
+        setLoading(false);
+      });
   }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <section className="currentWeather">
       <div className="currentWeather--details">
         <h2>Actual Temp</h2>
-        <span>0°</span>
+        <span>{(weather.main.temp - 273).toFixed(2)}</span>
         <ul className="currentWeather--stats">
           <li>
-            <span>Wind</span>
-            <span>5 km/h</span>
+            <span>Wind </span>
+            <span>{weather.wind.speed} km/h</span>
           </li>
           <li>
-            <span>Humidity</span>
-            <span>24%</span>
+            <span>Humidity: </span>
+            <span>{weather.main.humidity}%</span>
           </li>
         </ul>
-        <div className="currentWeather--actual">
-          <h3>Raining</h3>
-        </div>
-        <div className="currentWeather--actual">
-          <h3>Raining</h3>
-        </div>
-        <div className="currentWeather--actual">
-          <h3>Raining</h3>
-        </div>
-        <div className="currentWeather--actual">
-          <h3>Raining</h3>
-        </div>
         <div className="currentWeather--actual">
           <h3>Raining</h3>
         </div>
